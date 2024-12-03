aa45409158f02ab252bca5a4b70269075b6d7a4f
Bartolomeu <bartoszokroj@wp.pl>
2024-12-03 10:58:34 +0100

    Solution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Class Person
+   # Class Person
 
 - Read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md) before start
 
--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+
+    for person in people:
+        person_list.append(Person(person["name"], person["age"]))
+
+    for person_dict in people:
+
+        spouse_key = "husband" if person_dict.get("husband") is not None \
+            else "wife"
+        spouse_name = person_dict.get(spouse_key)
+
+        name = person_dict["name"]
+
+        if spouse_name is not None:
+            setattr(Person.people[name],
+                    spouse_key,
+                    Person.people[spouse_name])
+
+    return person_list
